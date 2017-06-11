5073427dbb63529184f833d43da1d6c02cd7499f
martio-dev <martiolabs@gmail.com>
2017-06-11 17:50:54 +0200

    testEmptyReceipt passed

--- src/main/java/it/mvtex/iws/t20170611/salestaxes/processing/ReceiptProcessor.java
+++ src/main/java/it/mvtex/iws/t20170611/salestaxes/processing/ReceiptProcessor.java
@@ -1,6 +1,12 @@
 package it.mvtex.iws.t20170611.salestaxes.processing;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public final class ReceiptProcessor {
+
+    private static final DecimalFormat PRICE_FORMATER = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.ENGLISH));
 
     public static final class Builder {
 
@@ -19,7 +25,14 @@ public final class ReceiptProcessor {
     }
 
     public String printReceiptDetails() {
-        return null;
+        StringBuilder resultBuilder = new StringBuilder();
+
+        final String systemNewLine = String.format("%n");
+
+        resultBuilder.append("Sales Taxes: ").append(PRICE_FORMATER.format(0)).append(systemNewLine);
+        resultBuilder.append("Total: ").append(PRICE_FORMATER.format(0)).append(systemNewLine);
+
+        return resultBuilder.toString();
     }
 
 }
