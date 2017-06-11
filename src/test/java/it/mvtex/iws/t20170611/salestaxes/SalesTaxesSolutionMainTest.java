877d787c6e87e8a539431944c3e0779cc73e4ebf
martio-dev <martiolabs@gmail.com>
2017-06-11 23:39:54 +0200

    test fix

--- src/test/java/it/mvtex/iws/t20170611/salestaxes/SalesTaxesSolutionMainTest.java
+++ src/test/java/it/mvtex/iws/t20170611/salestaxes/SalesTaxesSolutionMainTest.java
@@ -197,7 +197,7 @@ public class SalesTaxesSolutionMainTest
                 .addInvoices("1 imported bottle of perfume at 27.99",
                              "1 bottle of perfume at 18.99",
                              "1 packet of headache pills at 9.75",
-                             "1 box of imported chocolates at 11.25")
+                             "1 imported box of chocolates at 11.25")
                 .build()
                 .printReceiptDetails();
 
